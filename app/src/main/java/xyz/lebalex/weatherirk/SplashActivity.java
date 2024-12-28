3799212814a67704646486adbd9df4381b2942f8
lebalex <lebalex@mail.ru>
2024-12-28 13:43:48 +0800

    fix widget

--- app/src/main/java/xyz/lebalex/weatherirk/SplashActivity.java
+++ app/src/main/java/xyz/lebalex/weatherirk/SplashActivity.java
@@ -20,13 +20,17 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+
 /**
  * Created by ivc_lebedevav on 27.01.2017.
  */
 public class SplashActivity extends Activity {
 
     private SharedPreferences sp;
-    private String url = "https://www.lebalex.ru/meteo.json";
+    private String url = "https://lebalex.ru/meteo.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +53,7 @@ public class SplashActivity extends Activity {
 
     }
 
+
     private void getJsonFromUrl(String urls) {
 
         String resultJson = null;
@@ -57,7 +62,8 @@ public class SplashActivity extends Activity {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
             URL jsonUrl = new URL(urls);
-            URLConnection dc = jsonUrl.openConnection();
+            HttpsURLConnection dc =  (HttpsURLConnection)jsonUrl.openConnection();
+            dc.setHostnameVerifier(hostnameVerifier);
             dc.setConnectTimeout(5 * 1000);
             dc.setReadTimeout(5 * 1000);
             BufferedReader inputStream = new BufferedReader(new InputStreamReader(
@@ -69,15 +75,28 @@ public class SplashActivity extends Activity {
                     SharedPreferences.Editor editor = sp.edit();
                     editor.putString("meteo_url", jsonObject.getString("meteo_url"));
                     editor.apply();
+                }else
+                {
+                    LogWrite.LogError(getApplicationContext(), resultJson);
                 }
 
 
             } catch (JSONException e) {
+                LogWrite.LogError(getApplicationContext(), e.getMessage());
                 Log.e("SPLASH",e.getMessage());
             }
         } catch (Exception e4) {
+            LogWrite.LogError(getApplicationContext(), e4.getMessage());
             Log.e("SPLASH",e4.getMessage());
         }
     }
 
+    HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            HostnameVerifier hv =
+                    HttpsURLConnection.getDefaultHostnameVerifier();
+            return hv.verify("lebalex.ru", session);
+        }
+    };
 }
